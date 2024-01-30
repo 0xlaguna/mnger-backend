1681b5a3654abaddcb1a25c57a7f414e02654a76
0xlaguna <lagunamichael2@outlook.com>
2024-01-30 18:48:02 +0000

    feat(api): comment out api methods

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -30,118 +30,108 @@ pub mod routes;
 pub use entity::user;
 pub use entity::user::Entity as User;
 
-const DEFAULT_USERS_PER_PAGE: u64 = 5;
+// const DEFAULT_USERS_PER_PAGE: u64 = 5;
 
 
-#[post("/", data = "<user_form>")]
-async fn create(conn: Connection<'_, Db>, user_form: Form<user::Model>) -> Result<Json<user::Model>, Status> {
-    let db = conn.into_inner();
+// #[post("/", data = "<user_form>")]
+// async fn create(conn: Connection<'_, Db>, user_form: Form<user::Model>) -> Result<Json<user::Model>, Status> {
+//     let db = conn.into_inner();
 
-    let form = user_form.into_inner();
+//     let form = user_form.into_inner();
 
-    if Mutation::create_user(db, form.clone()).await.is_err() {
-        return Err(Status::InternalServerError);
-    }
+//     if Mutation::create_user(db, form.clone()).await.is_err() {
+//         return Err(Status::InternalServerError);
+//     }
 
-    Ok(Json(form))
-}
+//     Ok(Json(form))
+// }
 
-#[post("/<id>", data = "<user_form>")]
-async fn update(
-    conn: Connection<'_, Db>,
-    id: i32,
-    user_form: Form<user::Model>,
-) -> Flash<Redirect> {
-    let db = conn.into_inner();
+// #[post("/<id>", data = "<user_form>")]
+// async fn update(
+//     conn: Connection<'_, Db>,
+//     id: i32,
+//     user_form: Form<user::Model>,
+// ) -> Flash<Redirect> {
+//     let db = conn.into_inner();
 
-    let form = user_form.into_inner();
+//     let form = user_form.into_inner();
 
-    Mutation::update_user_by_id(db, id, form)
-        .await
-        .expect("could not update user");
+//     Mutation::update_user_by_id(db, id, form)
+//         .await
+//         .expect("could not update user");
 
-    Flash::success(Redirect::to("/"), "User successfully edited.")
-}
+//     Flash::success(Redirect::to("/"), "User successfully edited.")
+// }
 
-#[get("/?<page>&<users_per_page>")]
-async fn list(
-    conn: Connection<'_, Db>,
-    page: Option<u64>,
-    users_per_page: Option<u64>,
-    flash: Option<FlashMessage<'_>>,
-) -> Template {
-    let db = conn.into_inner();
+// #[get("/?<page>&<users_per_page>")]
+// async fn list(
+//     conn: Connection<'_, Db>,
+//     page: Option<u64>,
+//     users_per_page: Option<u64>,
+//     flash: Option<FlashMessage<'_>>,
+// ) -> Template {
+//     let db = conn.into_inner();
 
-    // Set page number and items per page
-    let page = page.unwrap_or(1);
-    let users_per_page = users_per_page.unwrap_or(DEFAULT_USERS_PER_PAGE);
-    if page == 0 {
-        panic!("Page number cannot be zero");
-    }
+//     // Set page number and items per page
+//     let page = page.unwrap_or(1);
+//     let users_per_page = users_per_page.unwrap_or(DEFAULT_USERS_PER_PAGE);
+//     if page == 0 {
+//         panic!("Page number cannot be zero");
+//     }
 
-    let (users, num_pages) = Query::find_users_in_page(db, page, users_per_page)
-        .await
-        .expect("Cannot find users in page");
+//     let (users, num_pages) = Query::find_users_in_page(db, page, users_per_page)
+//         .await
+//         .expect("Cannot find users in page");
 
-    Template::render(
-        "index",
-        json! ({
-            "page": page,
-            "users_per_page": users_per_page,
-            "num_pages": num_pages,
-            "users": users,
-            "flash": flash.map(FlashMessage::into_inner),
-        }),
-    )
-}
+//     Template::render(
+//         "index",
+//         json! ({
+//             "page": page,
+//             "users_per_page": users_per_page,
+//             "num_pages": num_pages,
+//             "users": users,
+//             "flash": flash.map(FlashMessage::into_inner),
+//         }),
+//     )
+// }
 
-#[get("/<id>")]
-async fn edit(conn: Connection<'_, Db>, id: i32) -> Template {
-    let db = conn.into_inner();
+// #[get("/<id>")]
+// async fn edit(conn: Connection<'_, Db>, id: i32) -> Template {
+//     let db = conn.into_inner();
 
-    let user: Option<user::Model> = Query::find_user_by_id(db, id)
-        .await
-        .expect("could not find user");
+//     let user: Option<user::Model> = Query::find_user_by_id(db, id)
+//         .await
+//         .expect("could not find user");
 
-    Template::render(
-        "edit",
-        json! ({
-            "user": user,
-        }),
-    )
-}
+//     Template::render(
+//         "edit",
+//         json! ({
+//             "user": user,
+//         }),
+//     )
+// }
 
-#[delete("/<id>")]
-async fn delete(conn: Connection<'_, Db>, id: i32) -> Flash<Redirect> {
-    let db = conn.into_inner();
+// #[delete("/<id>")]
+// async fn delete(conn: Connection<'_, Db>, id: i32) -> Flash<Redirect> {
+//     let db = conn.into_inner();
 
-    Mutation::delete_user(db, id)
-        .await
-        .expect("could not delete User");
+//     Mutation::delete_user(db, id)
+//         .await
+//         .expect("could not delete User");
 
-    Flash::success(Redirect::to("/"), "User successfully deleted.")
-}
+//     Flash::success(Redirect::to("/"), "User successfully deleted.")
+// }
 
-#[delete("/")]
-async fn destroy(conn: Connection<'_, Db>) -> Result<(), rocket::response::Debug<String>> {
-    let db = conn.into_inner();
+// #[delete("/")]
+// async fn destroy(conn: Connection<'_, Db>) -> Result<(), rocket::response::Debug<String>> {
+//     let db = conn.into_inner();
 
-    Mutation::delete_all_users(db)
-        .await
-        .map_err(|e| e.to_string())?;
+//     Mutation::delete_all_users(db)
+//         .await
+//         .map_err(|e| e.to_string())?;
 
-    Ok(())
-}
-
-#[catch(404)]
-pub fn not_found(req: &Request<'_>) -> Template {
-    Template::render(
-        "error/404",
-        json! ({
-            "uri": req.uri()
-        }),
-    )
-}
+//     Ok(())
+// }
 
 async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
     let conn = &Db::fetch(&rocket).unwrap().conn;
@@ -170,11 +160,6 @@ async fn start() -> Result<(), rocket::Error> {
     routes::mount(rocket)
         .attach(Db::init())
         .attach(AdHoc::try_on_ignite("Migrations", run_migrations))
-        .mount(
-            "/",
-            routes![create, delete, destroy, list, edit, update],
-        )
-        .register("/", catchers![not_found])
         .attach(Template::fairing())
         .manage(cors.clone())
         .launch()
