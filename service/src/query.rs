38f34bdd03cdb822cfa484e6c7241758702e1780
0xlaguna <lagunamichael2@outlook.com>
2024-01-30 18:49:50 +0000

    feat(service): rewrite query method

--- service/src/query.rs
+++ service/src/query.rs
@@ -1,13 +1,22 @@
 use ::entity::{user, user::Entity as User};
 use sea_orm::*;
 
+use crate::Result;
+use crate::{create_error, create_database_error};
+
 pub struct Query;
 
 impl Query {
-    pub async fn find_user_by_id(db: &DbConn, id: i32) -> Result<Option<user::Model>, DbErr> {
-        User::find_by_id(id).one(db).await
-    }
+    pub async fn find_user_by_id(db: &DbConn, id: i32) -> Result<user::Model> {
+        let user = User::find_by_id(id)
+            .one(db)
+            .await
+            .map_err(|err| create_database_error!(err))?
+            .ok_or_else(|| create_error!(NotFound))?;
 
+        Ok(user)
+    }
+    
     /// If ok, returns (user models, num pages).
     pub async fn find_users_in_page(
         db: &DbConn,
