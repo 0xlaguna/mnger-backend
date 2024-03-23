d1d0bbf77d050252b7b91b6e754176d2d61be6ea
0xlaguna <lagunamichael2@outlook.com>
2024-03-23 13:08:32 +0000

    fix: remove unused comments, add entry routes

--- nebula/src/routes/mod.rs
+++ nebula/src/routes/mod.rs
@@ -1,10 +1,12 @@
 use rocket::{Build, Rocket};
 
+mod index;
 mod users;
 mod account;
 
 pub fn mount(rocket: Rocket<Build>) -> Rocket<Build> {
     rocket
+        .mount("/", index::index_routes())
         .mount("/users", users::user_routes())
         .mount("/account", account::account_routes())
 }
