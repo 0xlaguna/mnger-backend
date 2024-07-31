e8c9f65d2f0cd0ad7a893bf58db0f533a5388364
0xlaguna <lagunamichael2@outlook.com>
2024-07-31 22:38:40 +0000

    fix: fix cors by using manual implementation

--- nebula/src/lib.rs
+++ nebula/src/lib.rs
@@ -3,9 +3,7 @@ extern crate rocket;
 
 use rocket_dyn_templates::Template;
 
-use rocket_cors::{AllowedOrigins, CorsOptions};
 use sea_orm_rocket::Database;
-use std::str::FromStr;
 
 use utoipa::OpenApi;
 use utoipa_rapidoc::RapiDoc;
@@ -17,37 +15,28 @@ use mnger_preon::r#impl::postgres::pool::Db;
 
 pub mod routes;
 pub mod docs;
+pub mod cors;
 
+
+#[options("/<_..>")]
+fn all_options() { }
 
 #[tokio::main]
-async fn start() -> Result<(), rocket::Error> {
-    let rocket = rocket::build();
-
-    // Configure CORS
-    let cors = CorsOptions {
-        allowed_origins: AllowedOrigins::All,
-        allowed_methods: [
-            "Get", "Put", "Post", "Delete", "Options", "Head", "Trace", "Connect", "Patch",
-        ]
-        .iter()
-        .map(|s| FromStr::from_str(s).unwrap())
-        .collect(),
-        ..Default::default()
-    }
-    .to_cors()
-    .expect("Failed to create CORS.");
-
-    routes::mount(rocket)
+async fn start()  -> Result<(), rocket::Error> {
+    let rocket = rocket::build()
+        .attach(Db::init())
+        .attach(Template::fairing())
+        .attach(cors::Cors)
+        .mount("/", routes![all_options])
         .mount(
             "/",
             SwaggerUi::new("/swagger-ui/<_..>").url("/api-docs/openapi.json", docs::ApiDoc::openapi()),
         )
         .mount("/", RapiDoc::new("/api-docs/openapi.json").path("/rapidoc"))
         .mount("/", Redoc::with_url("/redoc", docs::ApiDoc::openapi()))
-        .mount("/", Scalar::with_url("/scalar", docs::ApiDoc::openapi()))
-        .attach(Db::init())
-        .attach(Template::fairing())
-        .manage(cors.clone())
+        .mount("/", Scalar::with_url("/scalar", docs::ApiDoc::openapi()));
+
+    routes::mount(rocket)
         .launch()
         .await
         .map(|_| ())
